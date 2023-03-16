ac4aafce8a9582d4c1221c1501d62260f5d89bef
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-03-17 04:37:13 +0530

    mongoose product schema done

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose=require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect=require('./util/database').mongoConnect;
-const User=require('./models/user');
+// const User=require('./models/user');
 
 const app = express();
 
@@ -18,15 +18,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById("641351e21656b5786fb7b946")
-    .then(user => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+// app.use((req, res, next) => {
+//   User.findById("641351e21656b5786fb7b946")
+//     .then(user => {
+//       req.user = new User(user.name,user.email,user.cart,user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
 
-});
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,9 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-mongoConnect(()=>{
-  app.listen(3000);
+mongoose.connect('mongodb+srv://mohit:27mk2002@cluster0.wzw3ecy.mongodb.net/shop?retryWrites=true&w=majority')
+.then(()=>{
+app.listen(3000);
+}).catch(err=>{
+  console.log(err);
 })
